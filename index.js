cc01b24666ac3c61b2a1fe0da3d19ebcd7637bda
karon78 <karon78@gmail.com>
2020-07-11 18:02:17 +0300

    added class RangeValidator

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 "use strict";
+
+class RangeValidator {
+    constructor(from, to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    set from(v) {
+        if (typeof v !== 'number') {
+            throw new TypeError('param from must be a number');
+        }
+        this._from = v;
+    }
+
+    get from() {
+        return this._from;
+    }
+
+    set to(v) {
+        if (typeof v !== 'number') {
+            throw new TypeError('param to must be a number');
+        }
+        if (v < this.from) {
+            throw new RangeError('param to mast be bigger param from ');
+        }
+        this._to = v;
+    }
+
+    get to() {
+        return this._to;
+    }
+
+    get range() {
+        return `${this.from} - ${this.to}`;
+    }
+
+    static isValidate(v, obj) {
+        if (typeof v !== 'number') {
+            throw new TypeError('param from must be a number');
+        }
+        return v < obj.to || v > obj.from;
+
+    }
+}
+
+const objRange = new RangeValidator(12, 50);
+console.log(objRange.range);
+console.log(RangeValidator.isValidate(17, objRange));
